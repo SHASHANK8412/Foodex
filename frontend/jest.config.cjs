5016fefbec5c9cb62f66bbb92141efecbc399f23
K V Shashank <kadimishashank@gmail.com>
2026-04-20 11:27:11 +0530

    fix frontend jest test discovery

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+  testPathIgnorePatterns: ["<rootDir>/tests/e2e/"],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
